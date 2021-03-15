8b56e77060fdb5303f3ccb40675ecc264f10630a
Beata Salca-Sedevi <Beata.Sedevi@endava.com>
2021-03-15 16:21:54 +0200

    webdriverBea

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,33 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
+
+public class BaseTests {
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+
+        driver.get("https://the-internet.herokuapp.com/");
+
+        homePage = new HomePage(driver);
+
+    }
+
+
+    @AfterClass
+    public void TearDown(){
+        driver.quit();
+    }
+}
